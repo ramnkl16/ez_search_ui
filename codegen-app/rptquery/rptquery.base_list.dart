6a7ed5da72123a5bf59aa681ed2353889808607c
ram Duraisamy <ram.duraisamy@sbdinc.com>
2022-05-03 19:13:54 -0400

    fixed code generation logic

--- codegen-app/rptquery/rptquery.base_list.dart
+++ codegen-app/rptquery/rptquery.base_list.dart
@@ -1,16 +1,20 @@
-//Auto code generated from xml definition 2022-04-28 10:39:36.1586205 -0400 EDT
+//Auto code generated from xml definition 2022-05-03 19:12:06.7891109 -0400 EDT
 //RptQuery
-import 'package:ez_search_ui/common/base_cubit.dart';
-import 'package:ez_search_ui/common/global.dart';
 import 'package:ez_search_ui/constants/app_constant.dart';
-import 'package:ez_search_ui/helper/UIHelper.dart';
-import 'package:ez_search_ui/modules/rptquery/rptquery.cubit.dart';
 import 'package:ez_search_ui/modules/rptquery/rptquery.ds.dart';
 import 'package:ez_search_ui/constants/app_values.dart';
 import 'package:ez_search_ui/modules/rptquery/rptquery.model.dart';
+import 'package:ez_search_ui/modules/rptquery/rptquery.cubit.dart';
+import 'package:ez_search_ui/modules/rptquery/rptquery.edit.dart';
+import 'package:ez_search_ui/modules/booking_type/rptquery.data.model.dart';
+import 'package:ez_search_ui/modules/common/cubit/base_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ez_search_ui/common/global.dart';
 import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+import 'package:ez_search_ui/helper/UIhelper.dart';
+
+
 
 class RptQueryPage extends StatefulWidget {
   const RptQueryPage({Key? key}) : super(key: key);
@@ -24,13 +28,14 @@ class _RptQueryPageState extends State<RptQueryPage> {
   final DataGridController _dgController = DataGridController();
   List<RptQueryModel> list = <RptQueryModel>[];
 
+
   /// Determine to decide whether the device in landscape or in portrait.
   late bool isLandscapeInMobileView;
 
   @override
   void initState() {
-    BlocProvider.of<RptQueryCubit>(context).getAll();
-
+    BlocProvider.of<RptQueryListCubit>(context).getAll();
+     
     isWebOrDesktop = Global.isWeb || Global.isDesktop;
     _dgController.selectedIndex = 0;
     super.initState();
@@ -42,7 +47,7 @@ class _RptQueryPageState extends State<RptQueryPage> {
       body: Column(
         children: [
           _buildMenu(),
-          BlocBuilder<RptQueryCubit, BaseState>(
+          BlocBuilder<RptQueryListCubit, BaseState>(
             builder: (context, state) {
               print(state.runtimeType);
               if (state is BaseLoading) {
@@ -54,7 +59,7 @@ class _RptQueryPageState extends State<RptQueryPage> {
               } else if (state is BaseListSuccess<RptQueryModel>) {
                 list = state.list;
                 return _buildRptQueryGrid();
-              } else if (state is BaseEmpty) {
+              }else if (state is BaseEmpty) {
                 return Text("No record found, Please create a RptQuery");
               }
               return const Text("RptQuery has unknown state");
@@ -70,31 +75,30 @@ class _RptQueryPageState extends State<RptQueryPage> {
       children: [
         TextButton.icon(
             onPressed: () async {
-              if (_dgController.selectedIndex == -1) {
+              if (_dgController.selectedIndex == -1)
                 _dgController.selectedIndex = 0;
-              }
-              // var r =
-              //     await showEditPageDialog(list[_dgController.selectedIndex]);
-              // setState(() {});
+              var r =
+                  await showEditPageDialog(list[_dgController.selectedIndex]);
+              setState(() {});
             },
             icon: const Icon(Icons.edit),
             label: const Text(MenuConstants.edit)),
         TextButton.icon(
             onPressed: () async {
               var m = createNewModel();
-
-              // var r = await showEditPageDialog(m);
-              // print(m);
-              // if (r != null) {
-              //   list.add(m);
-              //   if (BlocProvider.of<RptQueryCubit>(context).state
-              //       is BaseEmpty) {
-              //     BlocProvider.of<RptQueryCubit>(context)
-              //         .emitInitialSuccess(list);
-              //   } else {
-              //     setState(() {});
-              //   }
-              // }
+              
+              var r = await showEditPageDialog(m);
+              print(m);
+              if (r != null) {
+                list.add(m);
+                if (BlocProvider.of<RptQueryListCubit>(context).state
+                    is BaseEmpty) {
+                  BlocProvider.of<RptQueryListCubit>(context)
+                      .emitInitialSuccess(list);
+                } else {
+                  setState(() {});
+                }
+              }
             },
             icon: const Icon(Icons.add),
             label: const Text(MenuConstants.newVal)),
@@ -127,63 +131,67 @@ class _RptQueryPageState extends State<RptQueryPage> {
     );
   }
 
-  // Future<dynamic> showEditPageDialog(RptQueryModel model) {
-  //   return showDialog(
-  //       context: context,
-  //       builder: (BuildContext context) {
-  //         return AlertDialog(
-  //           alignment: Alignment.center,
-  //           // children: [RptQueryEditPage()],
-  //           content: SizedBox(
-  //               width: MediaQuery.of(context).size.width - 100,
-  //               height: MediaQuery.of(context).size.height - 100,
-  //               child: BlocProvider(
-  //                 create: (context) => RptQueryEditCubit(),
-  //                 child: RptQueryEditPage(
-  //                   rp: model,
-  //                 ),
-  //               )),
-  //           // title: Text('data'),
-  //         );
-  //       });
-  // }
+  Future<dynamic> showEditPageDialog(RptQueryModel model) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            alignment: Alignment.center,
+            // children: [RptQueryEditPage()],
+            content: SizedBox(
+                width: MediaQuery.of(context).size.width - 100,
+                height: MediaQuery.of(context).size.height - 100,
+                child: BlocProvider(
+                  create: (context) => RptQueryEditCubit(),
+                  child: RptQueryEditPage(
+                    rp: model,
+                  ),
+                )),
+            // title: Text('data'),
+          );
+        });
+  }
 
   Widget _buildRptQueryGrid() {
     return Padding(
-      padding: EdgeInsets.all(AppValues.sfGridPadding),
+      padding:  EdgeInsets.all(AppValues.sfGridPadding),
       child: SfDataGrid(
-          allowSorting: true,
-          source: RptQueryDataGridSource(list),
-          selectionMode: SelectionMode.single,
-          allowPullToRefresh: true,
-          navigationMode: GridNavigationMode.cell,
-          controller: _dgController,
+      allowSorting: true,
+        source: RptQueryDataGridSource(list),
+        selectionMode: SelectionMode.single,
+        allowPullToRefresh: true,
+        navigationMode: GridNavigationMode.cell,
+        controller: _dgController,
 
-          // onSelectionChanging: (addedRows, removedRows) {
+        // onSelectionChanging: (addedRows, removedRows) {
 
-          // },
-          onCellTap: (DataGridCellDetails details) async {
-            _dgController.selectedIndex = details.rowColumnIndex.rowIndex - 1;
-            // if (details.rowColumnIndex.columnIndex == 0) {
-            //   await showEditPageDialog(list[_dgController.selectedIndex]);
-            //   setState(() {});
-            //   _dgController.selectedIndex = details.rowColumnIndex.rowIndex;
-            // }
-          },
-          columnWidthMode: isWebOrDesktop
-              ? (isWebOrDesktop && Global.isMobileResolution)
-                  ? ColumnWidthMode.none
-                  : ColumnWidthMode.fill
-              : isLandscapeInMobileView
-                  ? ColumnWidthMode.fill
-                  : ColumnWidthMode.none,
-          columns: <GridColumn>[
-            UIHelper.buildGridColumn(label: 'Name', columnName: 'name'),
-            UIHelper.buildGridColumn(label: 'Division', columnName: 'division'),
-            UIHelper.buildGridColumn(label: 'Page', columnName: 'page'),
-            UIHelper.buildGridColumn(label: 'Custom Data', columnName: 'cd'),
-          ]),
-    );
+        // },
+        onCellTap: (DataGridCellDetails details) async {
+          _dgController.selectedIndex = details.rowColumnIndex.rowIndex - 1;
+          if (details.rowColumnIndex.columnIndex == 0) {
+            await showEditPageDialog(list[_dgController.selectedIndex]);
+            setState(() {});
+            _dgController.selectedIndex = details.rowColumnIndex.rowIndex;
+          }
+        },
+        columnWidthMode: isWebOrDesktop
+            ? (isWebOrDesktop && Global.isMobileResolution)
+                ? ColumnWidthMode.none
+                : ColumnWidthMode.fill
+            : isLandscapeInMobileView
+                ? ColumnWidthMode.fill
+                : ColumnWidthMode.none,
+        columns: <GridColumn>[ 
+          
+         
+          UIHelper.buildGridColumn(label: 'Name', columnName: 'name'), 
+         
+          UIHelper.buildGridColumn(label: 'Division', columnName: 'division'), 
+         
+          UIHelper.buildGridColumn(label: 'Page', columnName: 'page'), 
+         
+          UIHelper.buildGridColumn(label: 'Custom Data', columnName: 'cd'),
+        ]),);
   }
 
   @override
@@ -194,12 +202,17 @@ class _RptQueryPageState extends State<RptQueryPage> {
   }
 
   RptQueryModel createNewModel() {
-    return RptQueryModel(
-      id: '',
-      name: '',
-      division: '',
-      page: '',
-      CustomData: '',
-    );
+    return RptQueryModel( 
+        id:  '', 
+         
+        name:  '', 
+         
+        division:  '', 
+         
+        page:  '', 
+         
+        cd:  '', 
+        
+      );  
   }
 }
--- codegen-app/user/user.base_list.dart
+++ codegen-app/user/user.base_list.dart
@@ -1,17 +1,20 @@
-//Auto code generated from xml definition 2022-04-28 10:39:36.1512244 -0400 EDT
+//Auto code generated from xml definition 2022-05-03 19:12:06.7838856 -0400 EDT
 //User
-
-import 'package:ez_search_ui/common/base_cubit.dart';
-import 'package:ez_search_ui/common/global.dart';
 import 'package:ez_search_ui/constants/app_constant.dart';
-import 'package:ez_search_ui/helper/UIHelper.dart';
 import 'package:ez_search_ui/modules/user/user.ds.dart';
 import 'package:ez_search_ui/constants/app_values.dart';
 import 'package:ez_search_ui/modules/user/user.model.dart';
 import 'package:ez_search_ui/modules/user/user.cubit.dart';
+import 'package:ez_search_ui/modules/user/user.edit.dart';
+import 'package:ez_search_ui/modules/booking_type/user.data.model.dart';
+import 'package:ez_search_ui/modules/common/cubit/base_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ez_search_ui/common/global.dart';
 import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+import 'package:ez_search_ui/helper/UIhelper.dart';
+
+
 
 class UserPage extends StatefulWidget {
   const UserPage({Key? key}) : super(key: key);
@@ -25,13 +28,14 @@ class _UserPageState extends State<UserPage> {
   final DataGridController _dgController = DataGridController();
   List<UserModel> list = <UserModel>[];
 
+
   /// Determine to decide whether the device in landscape or in portrait.
   late bool isLandscapeInMobileView;
 
   @override
   void initState() {
     BlocProvider.of<UserListCubit>(context).getAll();
-
+     
     isWebOrDesktop = Global.isWeb || Global.isDesktop;
     _dgController.selectedIndex = 0;
     super.initState();
@@ -55,7 +59,7 @@ class _UserPageState extends State<UserPage> {
               } else if (state is BaseListSuccess<UserModel>) {
                 list = state.list;
                 return _buildUserGrid();
-              } else if (state is BaseEmpty) {
+              }else if (state is BaseEmpty) {
                 return Text("No record found, Please create a User");
               }
               return const Text("User has unknown state");
@@ -71,31 +75,30 @@ class _UserPageState extends State<UserPage> {
       children: [
         TextButton.icon(
             onPressed: () async {
-              if (_dgController.selectedIndex == -1) {
+              if (_dgController.selectedIndex == -1)
                 _dgController.selectedIndex = 0;
-              }
-              // var r =
-              //     await showEditPageDialog(list[_dgController.selectedIndex]);
-              // setState(() {});
+              var r =
+                  await showEditPageDialog(list[_dgController.selectedIndex]);
+              setState(() {});
             },
             icon: const Icon(Icons.edit),
             label: const Text(MenuConstants.edit)),
         TextButton.icon(
             onPressed: () async {
               var m = createNewModel();
-
-              // var r = await showEditPageDialog(m);
-              // print(m);
-              // if (r != null) {
-              //   list.add(m);
-              //   if (BlocProvider.of<UserListCubit>(context).state
-              //       is BaseEmpty) {
-              //     BlocProvider.of<UserListCubit>(context)
-              //         .emitInitialSuccess(list);
-              //   } else {
-              //     setState(() {});
-              //   }
-              // }
+              
+              var r = await showEditPageDialog(m);
+              print(m);
+              if (r != null) {
+                list.add(m);
+                if (BlocProvider.of<UserListCubit>(context).state
+                    is BaseEmpty) {
+                  BlocProvider.of<UserListCubit>(context)
+                      .emitInitialSuccess(list);
+                } else {
+                  setState(() {});
+                }
+              }
             },
             icon: const Icon(Icons.add),
             label: const Text(MenuConstants.newVal)),
@@ -128,66 +131,69 @@ class _UserPageState extends State<UserPage> {
     );
   }
 
-  // Future<dynamic> showEditPageDialog(UserModel model) {
-  //   return showDialog(
-  //       context: context,
-  //       builder: (BuildContext context) {
-  //         return AlertDialog(
-  //           alignment: Alignment.center,
-  //           // children: [UserEditPage()],
-  //           content: SizedBox(
-  //               width: MediaQuery.of(context).size.width - 100,
-  //               height: MediaQuery.of(context).size.height - 100,
-  //               child: BlocProvider(
-  //                 create: (context) => UserEditCubit(),
-  //                 child: UserEditPage(
-  //                   us: model,
-  //                 ),
-  //               )),
-  //           // title: Text('data'),
-  //         );
-  //       });
-  // }
+  Future<dynamic> showEditPageDialog(UserModel model) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            alignment: Alignment.center,
+            // children: [UserEditPage()],
+            content: SizedBox(
+                width: MediaQuery.of(context).size.width - 100,
+                height: MediaQuery.of(context).size.height - 100,
+                child: BlocProvider(
+                  create: (context) => UserEditCubit(),
+                  child: UserEditPage(
+                    us: model,
+                  ),
+                )),
+            // title: Text('data'),
+          );
+        });
+  }
 
   Widget _buildUserGrid() {
     return Padding(
-      padding: EdgeInsets.all(AppValues.sfGridPadding),
+      padding:  EdgeInsets.all(AppValues.sfGridPadding),
       child: SfDataGrid(
-          allowSorting: true,
-          source: UserDataGridSource(list),
-          selectionMode: SelectionMode.single,
-          allowPullToRefresh: true,
-          navigationMode: GridNavigationMode.cell,
-          controller: _dgController,
+      allowSorting: true,
+        source: UserDataGridSource(list),
+        selectionMode: SelectionMode.single,
+        allowPullToRefresh: true,
+        navigationMode: GridNavigationMode.cell,
+        controller: _dgController,
 
-          // onSelectionChanging: (addedRows, removedRows) {
+        // onSelectionChanging: (addedRows, removedRows) {
 
-          // },
-          onCellTap: (DataGridCellDetails details) async {
-            _dgController.selectedIndex = details.rowColumnIndex.rowIndex - 1;
-            if (details.rowColumnIndex.columnIndex == 0) {
-              // await showEditPageDialog(list[_dgController.selectedIndex]);
-              // setState(() {});
-              // _dgController.selectedIndex = details.rowColumnIndex.rowIndex;
-            }
-          },
-          columnWidthMode: isWebOrDesktop
-              ? (isWebOrDesktop && Global.isMobileResolution)
-                  ? ColumnWidthMode.none
-                  : ColumnWidthMode.fill
-              : isLandscapeInMobileView
-                  ? ColumnWidthMode.fill
-                  : ColumnWidthMode.none,
-          columns: <GridColumn>[
-            UIHelper.buildGridColumn(label: 'Name', columnName: 'name'),
-            UIHelper.buildGridColumn(
-                label: 'First Name', columnName: 'firstName'),
-            UIHelper.buildGridColumn(
-                label: 'Last Name', columnName: 'lastName'),
-            UIHelper.buildGridColumn(label: 'Email', columnName: 'email'),
-            UIHelper.buildGridColumn(label: 'Mobile', columnName: 'mobile'),
-          ]),
-    );
+        // },
+        onCellTap: (DataGridCellDetails details) async {
+          _dgController.selectedIndex = details.rowColumnIndex.rowIndex - 1;
+          if (details.rowColumnIndex.columnIndex == 0) {
+            await showEditPageDialog(list[_dgController.selectedIndex]);
+            setState(() {});
+            _dgController.selectedIndex = details.rowColumnIndex.rowIndex;
+          }
+        },
+        columnWidthMode: isWebOrDesktop
+            ? (isWebOrDesktop && Global.isMobileResolution)
+                ? ColumnWidthMode.none
+                : ColumnWidthMode.fill
+            : isLandscapeInMobileView
+                ? ColumnWidthMode.fill
+                : ColumnWidthMode.none,
+        columns: <GridColumn>[ 
+          
+         
+          UIHelper.buildGridColumn(label: 'Name', columnName: 'name'), 
+         
+          UIHelper.buildGridColumn(label: 'First Name', columnName: 'firstName'), 
+         
+          UIHelper.buildGridColumn(label: 'Last Name', columnName: 'lastName'), 
+         
+          UIHelper.buildGridColumn(label: 'Email', columnName: 'email'), 
+         
+          UIHelper.buildGridColumn(label: 'Mobile', columnName: 'mobile'),
+        ]),);
   }
 
   @override
@@ -198,13 +204,19 @@ class _UserPageState extends State<UserPage> {
   }
 
   UserModel createNewModel() {
-    return UserModel(
-      id: '',
-      name: '',
-      firstName: '',
-      lastName: '',
-      email: '',
-      mobile: '',
-    );
+    return UserModel( 
+        id:  '', 
+         
+        name:  '', 
+         
+        firstName:  '', 
+         
+        lastName:  '', 
+         
+        email:  '', 
+         
+        mobile:  '', 
+        
+      );  
   }
 }
